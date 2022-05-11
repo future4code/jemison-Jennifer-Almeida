0bf86ac40965a5f3a5d482eb5782add6f602d4ae
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-05-10 23:22:38 -0300

    add-exercicios-em-contrucao

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,108 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    let tamanhoDoArray = array.length
+    // console.log(tamanhoDoArray);
+    return tamanhoDoArray
+}
+retornaTamanhoArray(array);
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    let arrayInvertido = array.reverse();
+    // console.log(arrayInvertido)
+    return arrayInvertido
+}
+retornaArrayInvertido(array);
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  let arrayOrdenado = array.sort();
+  return arrayOrdenado
+
+
+}
+retornaArrayOrdenado(arrayOrdenado)
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+    const par = array => array % 2 === 0
+    let numerosPares = array.filter(par)
+
+    //console.log(`Os números pares do array são: ${numerosPares}`)
+    return numerosPares
+}
+retornaNumerosPares(array)
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    const par = array => array % 2 === 0
+    let numerosPares = array.filter(par)
+    const numerosParesAoQuadrado = numerosPares
+}
+
+retornaNumerosParesElevadosADois(array)
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+
+}
