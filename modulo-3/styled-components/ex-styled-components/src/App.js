57c76832d3403db51903c796942491b0cdc85c04
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-07-05 11:28:38 -0300

    LabZap em construção

--- modulo-3/styled-components/ex-styled-components/src/App.js
+++ modulo-3/styled-components/ex-styled-components/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import LabZap from './img/LabZap.svg';
-import { Header,DivPai, Logo } from './style';
+import {
+  Header, DivPai, Logo, ContainerCentral, FaixasLaranjas, Conteudo, Remetente, Mensagem
+  , BotaoEnviar,Footer
+} from './style';
 
 
 function App() {
@@ -10,13 +13,28 @@ function App() {
       <Header>
         <Logo src={LabZap} className="App-logo" alt="Imagem logo Labenu" />
       </Header>
-      <section className='Container'>
-        <div className='faixas-laterais'>
-          <span></span>
-          <span></span>
-        </div>
 
-      </section>
+      <ContainerCentral>
+        <FaixasLaranjas />
+        <Conteudo>
+          <Remetente>
+            <label>Remetente: </label>
+            <input type="text"></input>
+          </Remetente>
+          <Mensagem>
+            <label>Mensagem: </label>
+            <input type="text"></input>
+          </Mensagem>
+          <BotaoEnviar>
+            <button>Enviar </button>
+          </BotaoEnviar>
+        </Conteudo>
+        <FaixasLaranjas />
+      </ContainerCentral>
+      <Footer>
+        <p>  Copyrigth &copy; 2022 Labenu All rights reserved. R Paris Leme, 215, Conjunto 820 Pinheiros - CEP: 05424-150</p>
+
+      </Footer>
     </DivPai>
   );
 }
--- modulo-3/styled-components/ex-styled-components/src/style.js
+++ modulo-3/styled-components/ex-styled-components/src/style.js
@@ -13,5 +13,67 @@ export const DivPai = styled.div`
 
 export const Logo = styled.img`
 display: block;
-    margin: 0 auto;
+margin: 0 auto;
+margin-top: 20px;
+`;
+
+export const ContainerCentral = styled.section `
+display: flex;
+justify-content: space-between;
+margin-top: 10px;
+`;
+
+export const FaixasLaranjas = styled.span `
+
+height: 80vh;
+width: 8vw;
+background-color: #ff8f00;
+`;
+
+export const Conteudo = styled.div `
+display: flex;
+align-items: flex-end;
+
+`;
+
+export const Remetente = styled.div `
+display: flex;
+padding: 30px;
+
+input{
+width: 160px;
+border-radius: 3px;
+box-shadow:  2px grey;
+margin-left: 5px;
+    
+}
+`;
+
+export const Mensagem = styled.div `
+display: flex;
+padding: 30px;
+margin-left:0;
+
+input{
+width: 300px;
+border-radius: 3px;
+box-shadow:  2px grey;
+margin-left: 5px;
+}
+`;
+
+export const BotaoEnviar = styled.div`
+
+display: flex;
+padding: 30px;
+border-radius: 3px;
+box-shadow:  2px grey;
+margin-left: 5px;
+`;
+
+export const Footer = styled.div`
+display: flex;
+justify-content: center;
+background-color: #9e9e9e;
+width: 100%;
 `;
