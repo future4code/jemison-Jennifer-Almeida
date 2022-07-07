e99f456ecf51a66aa1321344bc9a39387af03ca6
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-07-07 20:14:12 -0300

    caixa de cometários

--- modulo-3/estados-insta4/insta4/insta4/src/App.js
+++ modulo-3/estados-insta4/insta4/insta4/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components'
 import Post from './components/Post/Post';
 /*import Anna from './img/anna-capa.png';
@@ -12,13 +12,17 @@ const MainContainer = styled.div`
   align-items: center;
 `
 function App() {
-  const arrayPost = [
-    { nomeUsuario: "Paulinha", fotoUsuario: "https://picsum.photos/50/50", fotoPost: "https://picsum.photos/200/150" },
-    { nomeUsuario: "Anna Delvey", fotoUsuario:"https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY", fotoPost:"https://picsum.photos/200/152" },
-    { nomeUsuario: "Neff", fotoUsuario:"https://picsum.photos/50/56", fotoPost: "https://picsum.photos/50/28" }
-  ]
 
-  const postMapeado = arrayPost.map((item, index) => {
+  const [inputNome, setInputNome] = useState("")
+  const [inputComentario, setInputComentario] = useState("")
+  const [postMapeado, setpostMapeado] = useState([
+    { nomeUsuario: "Paulinha", fotoUsuario: "https://picsum.photos/50/50", fotoPost: "https://picsum.photos/200/150" },
+    { nomeUsuario: "Anna Delvey", fotoUsuario: "https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY", fotoPost: "https://picsum.photos/200/152" },
+    { nomeUsuario: "Neff", fotoUsuario: "https://picsum.photos/50/56", fotoPost: "https://picsum.photos/50/28" }
+  ])
+
+
+  const listaPostMapeado = postMapeado.map((item, index) => {
     return (
       <Post key={index}
         nomeUsuario={item.nomeUsuario}
@@ -27,10 +31,41 @@ function App() {
       />
     )
   })
+  const handleInputNome = (e) => {
+   setInputNome(e.target.value)
+  }
 
+  const handleInputComentario = (e) => {
+    setInputComentario(e.target.value)
+  }
+const addComentario = (e) =>{
+  e.preventDefault();
+  const novoCometario = {nome:inputNome, cometario:inputComentario}
+ const novaListaDeComentarios = [... postMapeado, novoCometario]
+ setpostMapeado(novaListaDeComentarios)
+}
   return (
     <MainContainer>
-      {postMapeado}
+      {listaPostMapeado}
+      <form>
+        <label>Nome: </label>
+        <input
+          placeholder='Insira seu nome'
+          value={inputNome}
+          onChange={handleInputNome}
+        />
+
+        <label>Comentário: </label>
+        <input
+          placeholder='Insira seu comentário'
+          value={inputComentario}
+          onChange={handleInputComentario}
+        />
+
+
+        <button onClick={addComentario}>Adicionar </button>
+
+      </form>
     </MainContainer>
 
 
