fdb45d69baa65e27e2388c7f31e771cb1ccf9d35
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-10-23 14:21:18 -0300

    Lista atualizada

--- modulo-5/lista-typescript/src/exercicio3.ts
+++ modulo-5/lista-typescript/src/exercicio3.ts
@@ -1,3 +1,8 @@
+/*Você foi contratado por um serviço de streaming para organizar o sistema de catálogos de filmes. 
+Cada filme possui 3 informações essenciais: 1. nome do filme; 2. ano de lançamento e 3. gênero do filme.
+ Os gêneros da plataforma se limitam aqueles encontrados no seguinte ENUM de gêneros: */
+
+
 enum GENERO {
     ACAO = "ação",
     DRAMA = "drama",
