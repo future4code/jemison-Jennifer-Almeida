a06de4436037ed450cd3d00540e0c77a14a7c8e6
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-11-01 22:28:16 -0300

    Fluxo de Dados

---
+++ modulo-5/fluxo-de-dados/src/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express'
+
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+// Exercício 1: Crie uma nova API do zero e desenvolva um endpoint de teste com método GET
+// no path “/test” que retorna a mensagem: “Servidor rodando na porta (colocar porta)!”. 
+
+app.get("/test", (req:Request, res:Response) => {          
+    res.status(200).send("Servidor rodando na porta 3003!")
+})
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
---
+++ modulo-5/fluxo-de-dados/src/type.ts
@@ -0,0 +1,5 @@
+export type allTypes  = {
+    id: string,
+    name: string,
+    price: number
+}
