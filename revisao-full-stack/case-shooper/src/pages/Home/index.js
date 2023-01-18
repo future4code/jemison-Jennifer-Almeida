e8b8ed90275a43f69e1b6525de9f186688d24287
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2023-01-17 21:34:19 -0300

    Inicio-FrontEnd

---
+++ revisao-full-stack/case-shooper/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form  from '../../components/Form/index';
+import Header from '../../components/Header';
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <Form/>
+        </div>
+    );
+}  
